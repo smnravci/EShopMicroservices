1afbcfe65a8846df91fed8fa189e2682a6abbe30
Semanur Avci <semanur@scribeaccounts.com>
2025-11-25 14:41:51 +0300

    Basket microservices with vertical slice architecture and CQRS

---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoints.cs
@@ -0,0 +1,25 @@
+﻿namespace Basket.API.Basket.StoreBasket
+{
+    public record StoreBasketRequest(ShoppingCart Cart);
+
+    public record StoreBasketResponse(string UserName);
+
+    public class StoreBasketEndpoints : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<StoreBasketCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<StoreBasketResponse>();
+                return Results.Created("$/basket/{response.UserName}", response);
+            })
+                .WithName("StoreBasket")
+                .Produces<StoreBasketResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Store basket for a user")
+                .WithDescription("Stores the shopping cart/basket for a specific user.");
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Exceptions/BasketNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace Basket.API.Exceptions
+{
+    public class BasketNotFoundException : NotFoundException
+    {
+        public BasketNotFoundException(string userName)
+            : base($"Basket with user name '{userName}' was not found.")
+        {
+        }
+
+    }
+}
